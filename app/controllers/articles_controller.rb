392fa5f751dd1eff0ce81dc39275443eab4222f3
Joseph Williams <josephwilliamsjs@gmail.com>
2021-05-12 11:43:00 -0400

    create article_params method to further dry up code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
   
   def create
     # strong parameters - whitelisting of data
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
      if @article.save
       flash[:notice] = "Article was created successfully"
     # redirects to show action
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -46,6 +46,10 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
   
   

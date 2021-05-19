692db4da372fc2600149c6584896c71e17c5afcb
Joseph Williams <josephwilliamsjs@gmail.com>
2021-05-19 12:21:29 -0400

    implement many-to-many association on backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   # means a title needs to be present for an article to be saved
   validates :title, presence: true, length: {minimum:6, maximum:100}
   validates :description, presence: true, length: {minimum:10, maximum:300}

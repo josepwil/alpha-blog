142cd371a8702b3b07ea61911dc8860d96a2699d
Joseph Williams <josephwilliamsjs@gmail.com>
2021-05-19 15:38:25 -0400

    add edit categories feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,21 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated succcessfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+  
   
   
   def index
